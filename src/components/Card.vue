55e6f76d3a79df4fa0fc1d47ff0082d3b6d279d8
AlexanderA02 <106035161+AlexanderA02@users.noreply.github.com>
2025-02-18 19:12:32 +0100

    Card component works, creates a card for every class. Cards can also be filtered based on difficulty, only showing those of a select difficulty.

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  name: String,
+  difficulty: String,
+  description: String,
+  recommendedStats: Array,
+})
+</script>
+
+<template>
+  <section>
+    <RouterLink :to="`/roll-stats/${props.name}`">
+      {{ props.name }}
+    </RouterLink>
+    <p>Difficulty: {{ props.difficulty }}</p>
+    <p>Description: {{ props.description }}</p>
+    <p v-for="stat in props.recommendedStats">Recommended primary stat: {{ stat }}</p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background-color: black;
+  color: white;
+  border: solid 2px red;
+  margin: 1em;
+}
+</style>
--- src/components/DndClassList.vue
+++ src/components/DndClassList.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
-import { RouterLink } from 'vue-router'
+import Card from './Card.vue'
 
 // props
 const props = defineProps({
@@ -29,21 +29,28 @@ onMounted(getClasses)
 </script>
 
 <template>
-  <p>Prop:{{ props.difficulty }}</p>
-  <!-- v-if gör att listan enbart renderas om dndClasses har ett värde annat än undefined, detta undviker en exeption. -->
-  <ul v-if="dndClasses">
-    <li v-if="difficulty !== 'all'" v-for="dndClass in filterOnDifficulty">
-      <!-- :to gör att allt efter = tolkas som JavaScript, annars tolkas det bara som en sträng. -->
-      <RouterLink :to="`/roll-stats/${dndClass.name}`">
-        {{ dndClass.name }}
-      </RouterLink>
-    </li>
-    <li v-else v-for="dndClass in dndClasses">
-      <RouterLink :to="`/roll-stats/${dndClass.name}`">
-        {{ dndClass.name }}
-      </RouterLink>
-    </li>
-  </ul>
+  <section v-if="dndClasses">
+    <!-- v-if gör att listan enbart renderas om dndClasses har ett värde annat än undefined, detta undviker en exeption. -->
+    <!-- [] används runt recommended-stats eftersom att man vanligtvis inte kan ha bindestreck i JavaScript variabel namn. -->
+    <Card
+      v-if="difficulty !== 'all'"
+      v-for="dndClass in filterOnDifficulty"
+      :key="dndClass.id"
+      :name="dndClass.name"
+      :difficulty="dndClass.difficulty"
+      :description="dndClass.description"
+      :recommended-stats="dndClass['recommended-stats']"
+    ></Card>
+    <Card
+      v-else
+      v-for="dndClass in dndClasses"
+      :key="dndClass.name"
+      :name="dndClass.name"
+      :difficulty="dndClass.difficulty"
+      :description="dndClass.description"
+      :recommended-stats="dndClass['recommended-stats']"
+    ></Card>
+  </section>
 </template>
 
 <style scoped></style>
