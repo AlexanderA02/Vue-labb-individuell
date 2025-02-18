3379499d7140258206d32808916a9a087a7a72f5
AlexanderA02 <106035161+AlexanderA02@users.noreply.github.com>
2025-02-18 20:44:48 +0100

    Custom event added when rolling dice. V-on used also to display result.

---
+++ src/components/Dice.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const emit = defineEmits(['diceRolled'])
+
+let result = ref(null)
+const rollDice = () => {
+  // console.log('Button clicked!')
+  result.value = Math.floor(Math.random() * 6) + 1
+}
+
+watch(result, (result) => {
+  // console.log('watched')
+  emit('diceRolled', result)
+})
+</script>
+
+<template>
+  <button @click="rollDice">Roll dice {{ result }}</button>
+</template>
+
+<style scoped></style>
--- src/views/RollStatsPage.vue
+++ src/views/RollStatsPage.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useDndClassesStore } from '@/pinia-store/store'
+import Dice from '@/components/Dice.vue'
 
 // useRoute() returnerar ett Objekt som representerar url:en. Detta är användbart då jag behöver få tillgång till params:en i url:en för att veta vilken länk som användaren har klickat på.
 const route = useRoute()
 const dndClassesStore = useDndClassesStore()
+const sum = ref(0)
 </script>
 
 <template>
@@ -13,6 +16,8 @@ const dndClassesStore = useDndClassesStore()
   <ul v-for="dndClass in dndClassesStore.dndClasses">
     <li>{{ dndClass.name }}</li>
   </ul>
+  <Dice @dice-rolled="(result) => (sum += result)"></Dice>
+  <p>{{ sum }}</p>
 </template>
 
 <style></style>
